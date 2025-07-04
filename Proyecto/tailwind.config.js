5c575fbf4508f014f06d086872845383ce2d3d19
SrPirson <fcp0009@alu.medac.com>
2025-07-04 13:00:40 +0200

    Tailwind Proyecto web

---
+++ Proyecto/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+      extend: {
+        backgroundImage: {
+          open: "url('./assets/hamburger-menu-open.svg')",
+          close: "url('./assets/hamburger-menu.svg')",
+        },
+      },
+    },
+  }
